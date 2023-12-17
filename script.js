c8578485a231f698dc2f6b08ec0fb19c354b6432
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-17 11:58:08 +0530

    Old Version added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 async function sendMessage() {
-    const userInput = document.getElementById('userInput').value.toLowerCase();
+    const userInput = document.getElementById('userInput').value;
     const messageList = document.getElementById('messageList');
     const thinkingBubble = document.getElementById('thinkingBubble');
+
+    // Clear input box text
+    clearInput();
 
     const userMessage = document.createElement('div');
     userMessage.className = 'message userMessage';
@@ -11,26 +14,54 @@ async function sendMessage() {
     thinkingBubble.style.display = 'inline';
     document.getElementById('status').textContent = 'Typing...';
 
-    const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
-    const openaiApiKey = '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';
+    // Check for specific queries
+    if (userInput.toLowerCase().includes('date') || userInput.toLowerCase().includes('time') || userInput.toLowerCase().includes('day')) {
+        // Handle date, time, day query
+        const currentDate = new Date().toLocaleString();
+        await typeBotResponse(messageList, thinkingBubble, `Current date and time: ${currentDate}`);
+    } else if (userInput.toLowerCase().includes('news')) {
+        // Handle news query
+        const newsApiKey = 'a1dcbaf052cd4e959ec5259eba1157db'; // Replace with your News API key
 
-    try {
-        // Check if the user input contains a query about the current date/time/date
-        if (userInput.includes('current date') || userInput.includes('current time') || userInput.includes('current day')) {
-            const currentDate = new Date().toLocaleString();
-            await typeBotResponse(messageList, thinkingBubble, `The current date and time is: ${currentDate}`);
-        }
-        // Check if the user input contains a query for live location
-        else if (userInput.includes('live location')) {
-            try {
-                const position = await getCurrentLocation();
-                const { latitude, longitude } = position.coords;
-                await typeBotResponse(messageList, thinkingBubble, `Your current location is: Latitude ${latitude}, Longitude ${longitude}`);
-            } catch (error) {
-                console.error(error);
-                await displayErrorMessage(messageList, thinkingBubble, 'Unable to retrieve live location.');
+        try {
+            const newsResponse = await fetch(`https://newsapi.org/v2/top-headlines?apiKey=${newsApiKey}`);
+            const newsData = await newsResponse.json();
+
+            if (newsData.articles && newsData.articles.length > 0) {
+                const topNews = newsData.articles[0].title;
+                await typeBotResponse(messageList, thinkingBubble, `Latest news: ${topNews}`);
+            } else {
+                throw new Error('No news articles found.');
             }
-        } else {
+        } catch (error) {
+            console.error(error);
+            await displayErrorMessage(messageList, thinkingBubble, 'Error fetching news.');
+        }
+    } else if (userInput.toLowerCase().includes('temperature')) {
+        // Handle temperature query using OpenWeatherMap API
+        const openWeatherApiKey = '4b08a31d0b102256e3becde9631af19d'; // Replace with your OpenWeatherMap API key
+
+        try {
+            const locationData = await getCurrentLocation();
+            const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${locationData.latitude}&lon=${locationData.longitude}&appid=${openWeatherApiKey}`);
+            const weatherData = await weatherResponse.json();
+
+            if (weatherData.main && weatherData.main.temp) {
+                const currentTemperature = weatherData.main.temp;
+                await typeBotResponse(messageList, thinkingBubble, `Current temperature: ${currentTemperature}°C`);
+            } else {
+                throw new Error('Temperature data not available.');
+            }
+        } catch (error) {
+            console.error(error);
+            await displayErrorMessage(messageList, thinkingBubble, 'Error fetching temperature.');
+        }
+    } else {
+        // Handle generic query using OpenAI API
+        const apiUrl = 'https://api.openai.com/v1/engines/gpt-3.5-turbo-1106/completions';
+        const openaiApiKey = 'OpenAI-API-KEY-HERE';
+
+        try {
             const response = await fetch(apiUrl, {
                 method: 'POST',
                 headers: {
@@ -51,18 +82,25 @@ async function sendMessage() {
             } else {
                 throw new Error('Invalid response from server');
             }
+        } catch (error) {
+            console.error(error);
+            await displayErrorMessage(messageList, thinkingBubble, error.message);
         }
-    } catch (error) {
-        console.error(error);
-        await displayErrorMessage(messageList, thinkingBubble, error.message);
     }
-
-    document.getElementById('userInput').value = '';
 }
 
-// Function to get the current location using Geolocation API
-function getCurrentLocation() {
+async function getCurrentLocation() {
     return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                resolve({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            },
+            error => {
+                reject(error);
+            }
+        );
     });
 }
