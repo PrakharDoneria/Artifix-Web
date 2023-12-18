3b7ee6a1624f57d259ff12b76d76c0f0a4f73b3c
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-18 16:17:18 +0530

    Trivia DB added

--- script.js
+++ script.js
@@ -27,26 +27,21 @@ async function displayErrorMessage(messageList, thinkingBubble, error) {
     document.getElementById('status').textContent = 'Online';
 }
 
-async function fetchWikipediaSummary(query) {
-    const formattedQuery = encodeURIComponent(query);
-    const wikipediaApiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&titles=${formattedQuery}&origin=*`;
+async function fetchTriviaQuestion() {
+    const triviaApiUrl = 'https://opentdb.com/api.php?amount=1&type=multiple';
 
     try {
-        const response = await fetch(wikipediaApiUrl);
+        const response = await fetch(triviaApiUrl);
         const data = await response.json();
 
-        const pageId = Object.keys(data.query.pages)[0];
-        const extract = data.query.pages[pageId]?.extract;
-
-        if (extract) {
-            const summary = extract.replace(/<\/?[^>]+(>|$)/g, '').split('\n')[0];
-            return summary.trim() !== '' ? summary : null;
+        if (data.results && data.results.length > 0) {
+            return data.results[0].question;
         } else {
-            throw new Error('No information found on Wikipedia.');
+            throw new Error('Error fetching trivia question.');
         }
     } catch (error) {
         console.error(error);
-        throw new Error('Error fetching information from Wikipedia.');
+        throw new Error('Error fetching trivia question.');
     }
 }
 
@@ -63,103 +58,62 @@ async function sendMessage() {
     thinkingBubble.style.display = 'inline';
     document.getElementById('status').textContent = 'Typing...';
 
-    const ipinfoApiKey = '7ccae9c8d8744e';
-    const ipinfoUrl = `https://ipinfo.io/json?token=${ipinfoApiKey}`;
-
     try {
-        const ipinfoResponse = await fetch(ipinfoUrl);
-        const ipinfoData = await ipinfoResponse.json();
-        const userCity = ipinfoData.city;
-
         if (userInput.toLowerCase().includes('day today') || userInput.toLowerCase().includes('time') || userInput.toLowerCase().includes('date today')) {
             const currentDate = new Date();
             const response = `Today is ${currentDate.toDateString()} and the time is ${currentDate.toLocaleTimeString()}.`;
             await typeBotResponse(messageList, thinkingBubble, response);
-        } else if (userInput.toLowerCase().includes('news')) {
-            const newsApiKey = 'a1dcbaf052cd4e959ec5259eba1157db';
-            const country = 'us';
-            const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${newsApiKey}`;
-
-            try {
-                const newsResponse = await fetch(newsApiUrl);
-                const newsData = await newsResponse.json();
-
-                if (newsData.status === 'ok') {
-                    const articles = newsData.articles.slice(0, 3);
-                    const response = articles.map(article => `${article.title} - ${article.url}`).join('\n');
-                    await typeBotResponse(messageList, thinkingBubble, response);
-                } else {
-                    throw new Error('Error fetching news.');
-                }
-            } catch (error) {
-                console.error(error);
-                await displayErrorMessage(messageList, thinkingBubble, 'Error fetching news.');
-            }
         } else if (userInput.toLowerCase().includes('temperature')) {
+            // Weather API code (unchanged)
+            const ipinfoApiKey = '7ccae9c8d8744e';
+            const ipinfoUrl = `https://ipinfo.io/json?token=${ipinfoApiKey}`;
+
+            const ipinfoResponse = await fetch(ipinfoUrl);
+            const ipinfoData = await ipinfoResponse.json();
+            const userCity = ipinfoData.city;
+
             const weatherApiKey = '4b08a31d0b102256e3becde9631af19d';
             const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${weatherApiKey}`;
 
-            try {
-                const weatherResponse = await fetch(weatherApiUrl);
-                const weatherData = await weatherResponse.json();
-                const temperatureKelvin = weatherData.main.temp;
-                const temperatureCelsius = temperatureKelvin - 273.15;
-                const roundedTemperature = Math.round(temperatureCelsius);
-                const response = `The current temperature in ${userCity} is ${roundedTemperature}°C.`;
+            const weatherResponse = await fetch(weatherApiUrl);
+            const weatherData = await weatherResponse.json();
+            const temperatureKelvin = weatherData.main.temp;
+            const temperatureCelsius = temperatureKelvin - 273.15;
+            const roundedTemperature = Math.round(temperatureCelsius);
+            const response = `The current temperature in ${userCity} is ${roundedTemperature}°C.`;
 
-                await typeBotResponse(messageList, thinkingBubble, response);
-            } catch (error) {
-                console.error(error);
-                await displayErrorMessage(messageList, thinkingBubble, 'Error fetching temperature.');
-            }
-        } else if (userInput.toLowerCase().includes('who is')) {
-            const personQuery = userInput.toLowerCase().replace('who is', '').trim();
-
-            try {
-                const wikipediaSummary = await fetchWikipediaSummary(personQuery);
-
-                if (wikipediaSummary !== null) {
-                    await typeBotResponse(messageList, thinkingBubble, wikipediaSummary);
-                } else {
-                    throw new Error('No relevant information found on Wikipedia.');
-                }
-            } catch (error) {
-                console.error(error);
-                await displayErrorMessage(messageList, thinkingBubble, error.message);
-            }
+            await typeBotResponse(messageList, thinkingBubble, response);
+        } else if (userInput.toLowerCase().includes('homework') || userInput.toLowerCase().includes('question')) {
+            // Trivia question API code
+            const triviaQuestion = await fetchTriviaQuestion();
+            await typeBotResponse(messageList, thinkingBubble, `Sure, here's a trivia question for you:\n${triviaQuestion}`);
         } else {
+            // OpenAI GPT-3 code (unchanged)
             const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
             const openaiApiKey = 'OpenAI-API-KEY';
 
-            try {
-                const response = await fetch(apiUrl, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${openaiApiKey}`,
-                    },
-                    body: JSON.stringify({
-                        prompt: userInput,
-                        max_tokens: 50,
-                        temperature: 0.5,
-                    }),
-                }).then(res => res.json());
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${openaiApiKey}`,
+                },
+                body: JSON.stringify({
+                    prompt: userInput,
+                    max_tokens: 50,
+                    temperature: 0.5,
+                }),
+            }).then(res => res.json());
 
-                console.log(response);
-
-                if (response.choices && response.choices.length > 0) {
-                    await typeBotResponse(messageList, thinkingBubble, response.choices[0].text);
-                } else {
-                    throw new Error('Invalid response from server');
-                }
-            } catch (error) {
-                console.error(error);
-                await displayErrorMessage(messageList, thinkingBubble, error.message);
+            if (response.choices && response.choices.length > 0) {
+                await typeBotResponse(messageList, thinkingBubble, response.choices[0].text);
+            } else {
+                throw new Error('Invalid response from server');
             }
         }
     } catch (error) {
         console.error(error);
-        await displayErrorMessage(messageList, thinkingBubble, 'Error fetching user location.');
+        await displayErrorMessage(messageList, thinkingBubble, error.message);
     }
 
     document.getElementById('userInput').value = '';
@@ -196,6 +150,6 @@ function populateChatHistory() {
     messageList.scrollTop = messageList.scrollHeight;
 }
 
+// Populate chat history and save on page unload
 populateChatHistory();
-
 window.addEventListener('beforeunload', () => saveChatHistory());
